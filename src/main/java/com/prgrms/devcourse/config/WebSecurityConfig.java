f6c0b8dbba16068418f72d591739955d10ec92e7
kkangh00n <kig2454@gmail.com>
2023-11-20 19:33:15 +0900

    feat: Spring Security ignoring 처리

--- src/main/java/com/prgrms/devcourse/config/WebSecurityConfig.java
+++ src/main/java/com/prgrms/devcourse/config/WebSecurityConfig.java
@@ -13,11 +13,13 @@ public class WebSecurityConfig{
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        //정적 리소스에 대한 필터기능 해제
         return (web) -> web.ignoring().requestMatchers("/assets/**");
     }
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //시큐리티 필터 기능
         http
             .authorizeHttpRequests((authorizeRequests) ->
                 authorizeRequests
