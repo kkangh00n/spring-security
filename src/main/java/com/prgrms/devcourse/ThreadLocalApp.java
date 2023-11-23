9bb7fc880058a318f52089a78542bd851ac9129f
kkangh00n <kig2454@gmail.com>
2023-11-23 18:23:52 +0900

    feat: ThreadLocal

---
+++ src/main/java/com/prgrms/devcourse/ThreadLocalApp.java
@@ -0,0 +1,40 @@
+package com.prgrms.devcourse;
+
+import static java.util.concurrent.CompletableFuture.*;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ThreadLocalApp {
+
+    final static ThreadLocal<Integer> threadLocalValue = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        System.out.println(getCurrentThreadName() + " ### main set value = 1");
+        threadLocalValue.set(1);
+
+        a();
+        b();
+
+        CompletableFuture<Void> task = runAsync(() -> {
+            a();
+            b();
+        });
+
+        task.join();
+    }
+
+    public static void a(){
+        Integer value = threadLocalValue.get();
+        System.out.println(getCurrentThreadName() + " ### a() get value = " + value);
+    }
+
+    public static void b(){
+        Integer value = threadLocalValue.get();
+        System.out.println(getCurrentThreadName() + " ### b() get value = " + value);
+    }
+
+    public static String getCurrentThreadName(){
+        return Thread.currentThread().getName();
+    }
+
+}
