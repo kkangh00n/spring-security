a002683724a5f1fad261de6ae043ff66ecfb0a4e
kkangh00n <kig2454@gmail.com>
2023-12-01 15:04:31 +0900

    feat: log4jdbc 설정

---
+++ src/main/java/com/prgrms/devcourse/config/DataSourcePostProcessor.java
@@ -0,0 +1,20 @@
+package com.prgrms.devcourse.config;
+
+import javax.sql.DataSource;
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataSourcePostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource && !(bean instanceof Log4jdbcProxyDataSource)) {
+            return new Log4jdbcProxyDataSource((DataSource) bean);
+        } else {
+            return bean;
+        }
+    }
+}
